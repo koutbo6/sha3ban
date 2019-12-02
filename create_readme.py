e0c0ef98250fe0953ee3d04c8cbfb1fa5ec2039b
Mohammad AlMarzouq <mohammadalmarzouq@Mohammads-iMac.local>
2019-12-02 19:49:05 +0300

    update files

--- create_readme.py
+++ create_readme.py
@@ -2,17 +2,44 @@ import os
 
 print("## مذكرات الشيخ شعبان علي في علم التجويد")
 EXTS_TO_DISPLAY = [".pdf", ".docx", ".doc", ]
-file_names = set([os.path.splitext(p)[0] for p in os.listdir(".")])
 
-# fix names with space
-for f in file_names:
-    for ext in EXTS_TO_DISPLAY:
-        fpath = "{}{}".format(f, ext)
+FILES_TO_EXCLUDE = [".DS_Store"]
+DIRS_TO_EXCLUDE = ["pdfs", ".git"]
+def list_files_in_dir(fpath):
+    my_files = [os.path.splitext(p)[0] for p in os.listdir(fpath) if not os.path.isdir(p)]
+    return set([x for x in my_files if x not in FILES_TO_EXCLUDE])
+
+def list_dirs_in_dir(fpath):
+    my_dirs = [x for x in os.listdir(".") if os.path.isdir(x)]
+    return set([x for x in my_dirs if x not in DIRS_TO_EXCLUDE])
+
+def fix_filenames_with_space(flist):
+    # fix names with space
+    for f in flist:
+        for ext in EXTS_TO_DISPLAY:
+            fpath = "{}{}".format(f, ext)
+            if os.path.exists(fpath) and " " in fpath:
+                os.rename(fpath, fpath.replace(" ", "_"))
+
+def fix_dirnames_with_space(dlist):
+    # fix names with space
+    for fpath in dlist:
         if os.path.exists(fpath) and " " in fpath:
             os.rename(fpath, fpath.replace(" ", "_"))
+                
+
+file_names = list_files_in_dir(".")
+directories = list_dirs_in_dir(".")
+
+fix_filenames_with_space(file_names)
+fix_dirnames_with_space(directories)
 
 # get the new names
-file_names = set([os.path.splitext(p)[0] for p in os.listdir(".")])
+#file_names = set([os.path.splitext(p)[0] for p in os.listdir(".")])
+
+current_working_dir = os.getcwd()
+file_names = list_files_in_dir(".")
+# change working dir using os.chdir()
 
 # create readme
 for f in file_names:
@@ -24,3 +51,8 @@ for f in file_names:
                 ext[1:],
                 fpath))
             break
+
+# for d in directories:
+#     dpath = "./{}".format(d)
+#     file_names = list_files_in_dir(dpath)
+#     fix_filenames_with_space(file_names)
